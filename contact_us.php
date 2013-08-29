bbfc6745968fadd73b73f0160306039054ede004
kevin <sujimedayil@gmail.com>
2013-08-30 00:29:50 +0530

    contact_us.php page pushed

---
+++ contact_us.php
@@ -0,0 +1,56 @@
+
+<div class="tabbable">
+  <!-- Only required for left/right tabs -->
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1">
+      <h3>Contact Us</h3>
+      <p>&nbsp;</p>
+      <div>
+        <table border="0">
+          <tr>
+            <td width="92"><b>Address : </b></td>
+            <td width="191"> ST.ANN'S SCHOOL</td>
+          </tr>
+          <tr>
+            <td rowspan="4"></td>
+            <td><h5>( Affiliated to CBSE, No.930688 )</h5></td>
+          </tr>
+          <tr>
+            <td>AYOOR- 691 533</td>
+          </tr>
+          <tr>
+            <td>KOLLAM (Dist.)</td>
+          </tr>
+          <tr>
+            <td>KERALA</td>
+          </tr>
+        </table>
+		
+		 &nbsp;
+		 
+		<!--Email,Fax and Phone-->
+		
+		  <table border="0">
+          <tr>
+            <td width="93" height="37"><b>E-mail : </b></td>
+            <td width="190"> stannsayur@yahoo.co.in</td>
+          </tr>
+           <tr>
+            <td width="93" height="37"><b>Ph. No : </b></td>
+            <td width="190"> 0475 2294485</td>
+          </tr>
+		   <tr>
+            <td width="93" height="34"><b>Fax No : </b></td>
+            <td width="190"> 9856473625225</td>
+          </tr>
+          
+        </table>
+      </div>
+    </div>
+    <div class="tab-pane" id="tab4"></div>
+    <div class="tab-pane center" id="tab5">
+      <h3>&nbsp;</h3>
+      <h4>&nbsp;</h4>
+    </div>
+  </div>
+</div>
