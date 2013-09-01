6a646f3031efc8012df89e64d15f684b07b21fee
vini <vineethtrv@gmail.com>
2013-09-01 14:04:04 +0530

    students page added

---
+++ students.php
@@ -0,0 +1,40 @@
+<div class="hm-content">
+	<h3>Students</h3>
+	<table class="table table-hover">
+        <thead>
+		
+			<tr class="info">
+			  <th>Class</th>
+			  <th>No.Boys</th>
+			  <th>No. Girls</th>
+			  <th>Total Students</th>
+			</tr>
+       </thead>
+       <tbody>
+			<tr>
+			  <td>1</td>
+			  <td>10</td>
+			  <td>30</td>
+			  <td>40</td>
+			</tr>
+			<tr>
+			  <td>2</td>
+			  <td>32</td>
+			  <td>25</td>
+			  <td>57</td>
+			</tr>
+			<tr>
+			  <td>3</td>
+			  <td>10</td>
+			  <td>30</td>
+			  <td>40</td>
+			</tr>
+			<tr>
+			  <td>4</td>
+			  <td>32</td>
+			  <td>25</td>
+			  <td>57</td>
+			</tr>
+			
+   </tbody>
+   </table>
