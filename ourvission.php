88bd7b1f518e93d77e4d355d128f75fc6e46fd52
vini <vineethtrv@gmail.com>
2013-09-01 17:29:23 +0530

    vision page added

---
+++ ourvission.php
@@ -0,0 +1,62 @@
+            <h3>About Us</h3>
+<div class="hm-content">
+	<h4>History</h4>
+      <img src="<?php echo $resourcePath; ?>img/layout/image1.jpg" class="pcr-lrg" />
+            <p align="justify"> The three schools, two junior and one senior,
+                Foundation in Worcester make up one of the leading academic institutions in the West Midlands and offer an outstanding educational experience.
+                The Foundation was inspected at the end of 2011 and you can download here a copy of the official report from ISI.
+                The senior school has one of the finest settings of any city school in the country.
+                It borders the River Severn in the heart of Worcester and looks across to the Malvern Hills.
+                It is adjacent to the Cathedral in an oasis of calm, and yet two minutes'
+                walk from the pedestrianised city centre and within easy walking distance of train and bus stations.
+                One of the junior schools, King's St Alban's, shares this tranquillity by being next to the senior site;
+                the other, King's Hawford, is in a more rural setting just north of the city.
+                The three schools, two junior and one senior,
+			</p>
+			<div class="mang-thump">
+			<img src="<?php echo $resourcePath; ?>img/layout/man.jpg"/>
+			</div>
+			<p align="justify"> 
+                Foundation in Worcester make up one of the leading academic institutions in the West Midlands and offer an outstanding educational experience.
+                The Foundation was inspected at the end of 2011 and you can download here a copy of the official report from ISI.
+                The senior school has one of the finest settings of any city school in the country.
+                It borders the River Severn in the heart of Worcester and looks across to the Malvern Hills.
+                It is adjacent to the Cathedral in an oasis of calm, and yet two minutes'
+                walk from the pedestrianised city centre and within easy walking distance of train and bus stations.
+                One of the junior schools, King's St Alban's, shares this tranquillity by being next to the senior site;
+                the other, King's Hawford, is in a more rural setting just north of the city.
+				Foundation in Worcester make up one of the leading academic institutions in the West Midlands and offer an outstanding educational experience.
+                The Foundation was inspected at the end of 2011 and you can download here a copy of the official report from ISI.
+                The senior school has one of the finest settings of any city school in the country.
+                It borders the River Severn in the heart of Worcester and looks across to the Malvern Hills.
+                It is adjacent to the Cathedral in an oasis of calm, and yet two minutes'
+                walk from the pedestrianised city centre and within easy walking distance of train and bus stations.
+                One of the junior schools, King's St Alban's, shares this tranquillity by being next to the senior site;
+                the other, King's Hawford, is in a more rural setting just north of the city.
+			</p>
+			<div class="clear"></div>
+</div>
+<div class="hm-content">
+	<h4>Our Vision </h4>
+      <img src="<?php echo $resourcePath; ?>img/layout/school.jpg" class="pcr" />
+            <p align="justify"> The three schools, two junior and one senior,
+                Foundation in Worcester make up one of the leading academic institutions in the West Midlands and offer an outstanding educational experience.
+                The Foundation was inspected at the end of 2011 and you can download here a copy of the official report from ISI.
+                The senior school has one of the finest settings of any city school in the country.
+                It borders the River Severn in the heart of Worcester and looks across to the Malvern Hills.
+                It is adjacent to the Cathedral in an oasis of calm, and yet two minutes'
+                walk from the pedestrianised city centre and within easy walking distance of train and bus stations.
+                One of the junior schools, King's St Alban's, shares this tranquillity by being next to the senior site;
+                the other, King's Hawford, is in a more rural setting just north of the city.
+                The three schools, two junior and one senior,
+                Foundation in Worcester make up one of the leading academic institutions in the West Midlands and offer an outstanding educational experience.
+                The Foundation was inspected at the end of 2011 and you can download here a copy of the official report from ISI.
+                The senior school has one of the finest settings of any city school in the country.
+                It borders the River Severn in the heart of Worcester and looks across to the Malvern Hills.
+                It is adjacent to the Cathedral in an oasis of calm, and yet two minutes'
+                walk from the pedestrianised city centre and within easy walking distance of train and bus stations.
+                One of the junior schools, King's St Alban's, shares this tranquillity by being next to the senior site;
+                the other, King's Hawford, is in a more rural setting just north of the city.
+			</p>
+</div>
+       
